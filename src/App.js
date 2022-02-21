1925f92b4cc28c7d083da4678bf1f82e5bfece6c
legolas2k16 <legolas2k16@gmail.com>
2022-02-21 08:56:56 +0530

    Some Changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./components/Card";
+import About from "./components/About";
+
+function App() {
+  <div className="min-h-screen py-10 px-3 sm:px-5 bg-grey-100">
+    <Card/>
+    <About/>
+  </div>
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="max-w-4xl mt-20 mx-auto">
+      <p className="text-2xl text-black md:text-4xl font-bold text-center">
+        Hello 😑
+      </p>
+      {/* <p className="text-base text-center leading-relaxed mt-4">
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but
+        also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s
+      </p> */}
+    </div>
+  );
+}
+
+export default Card;
